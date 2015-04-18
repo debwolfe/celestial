0d6e4706b38719a6ec8c07c4511758cc1cf08047
Dixon Adair <dixonadairv@gmail.com>
2015-04-18 16:17:15 -0700

    webgl done with mapbox commented out

---
+++ app/assets/javascripts/mapbox.js
@@ -0,0 +1,15 @@
+$(function() {
+
+	// // var popupDescription = "";
+
+	// // Declare mapbox access token and set up map
+	// L.mapbox.accessToken = 'pk.eyJ1IjoiZGl4b25hZGFpciIsImEiOiJhR1dMRERVIn0.fpbNVrP6tCB3jOKzm41vlA' // my access token
+
+	// // Define/Grab values for some variables
+	// var geolocate = $('#geolocate'); // the "find me" button
+	// var current_location_map = L.mapbox.map('current_location_map', 'dixonadair.lkbob6kb');
+	// var myLayer = L.mapbox.featureLayer().addTo(current_location_map);
+
+	// // set map view
+	// current_location_map.setView([37.734585, -122.447214], 12);
+}
