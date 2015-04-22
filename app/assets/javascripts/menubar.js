cca9708005d26cfc538c58dc5c92c37c795c3f42
Deb Wolfe <deblogic@gmail.com>
2015-04-22 16:57:22 -0700

    navbar moving smoothly and working on first page

--- app/assets/javascripts/menubar.js
+++ app/assets/javascripts/menubar.js
@@ -1,18 +1,38 @@
 function cssMenu() {
-	// $(window).on('scroll', function() {
- //    if ($(window).scrollTop() > $(window).height()) {
- //    	$('#cssmenu').css('top', '0px');
- //    	$('#cssmenu').css('position', 'fixed');
- //    } else {
- //    	$('#cssmenu').css('top', '0px');
- //    	$('#cssmenu').css('position', 'absolute');
- //    };
+	$(window).on('scroll', function() {
     for (i=0; i<5; i++) {
-        if ($(window).scrollTop() > 700*i && $(window).scrollTop() < 700*(i+1)) {
+        if ($(window).scrollTop() > $(window).height()*i && $(window).scrollTop() < $(window).height()*(i+1)) {
             $('#cssmenu ul li').removeClass('active');
             $('#cssmenu ul li:nth-child('+ (i+1) +')').addClass('active');
         };
     };
+
+    $('#cssmenu ul li:nth-child(1)').on('click', function() {
+      $('#cssmenu ul li').removeClass('active');
+      $('#cssmenu ul li:nth-child(1)').addClass('active');
+    });
+
+    $('#cssmenu ul li:nth-child(2)').on('click', function() {
+      $('#cssmenu ul li').removeClass('active');
+      $('#cssmenu ul li:nth-child(2)').addClass('active');
+    });
+
+    $('#cssmenu ul li:nth-child(3)').on('click', function() {
+      $('#cssmenu ul li').removeClass('active');
+      $('#cssmenu ul li:nth-child(3)').addClass('active');
+    });
+
+    $('#cssmenu ul li:nth-child(4)').on('click', function() {
+      $('#cssmenu ul li').removeClass('active');
+      $('#cssmenu ul li:nth-child(4)').addClass('active');
+    });
+
+    $('#cssmenu ul li:nth-child(5)').on('click', function() {
+      $('#cssmenu ul li').removeClass('active');
+      $('#cssmenu ul li:nth-child(5)').addClass('active');
+    });
+
+  });
 };
 
 $(function(){cssMenu()});
