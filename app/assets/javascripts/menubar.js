a0b9279299058fa631424de9c580baf99fbf8ea6
Kailash <kaidurai@gmail.com>
2015-04-22 10:37:42 -0700

    append css to top of page

--- app/assets/javascripts/menubar.js
+++ app/assets/javascripts/menubar.js
@@ -1,12 +1,12 @@
 function cssMenu() {
-	$(window).on('scroll', function() {
-    if ($(window).scrollTop() > $(window).height()) {
-    	$('#cssmenu').css('top', '0px');
-    	$('#cssmenu').css('position', 'fixed');
-    } else {
-    	$('#cssmenu').css('top', $(window).height());
-    	$('#cssmenu').css('position', 'absolute');
-    };
+	// $(window).on('scroll', function() {
+ //    if ($(window).scrollTop() > $(window).height()) {
+ //    	$('#cssmenu').css('top', '0px');
+ //    	$('#cssmenu').css('position', 'fixed');
+ //    } else {
+ //    	$('#cssmenu').css('top', '0px');
+ //    	$('#cssmenu').css('position', 'absolute');
+ //    };
     for (i=0; i<5; i++) {
     	if ($(window).scrollTop() > 700*i && $(window).scrollTop() < 700*(i+1)) {
     		$('#cssmenu ul li').removeClass('active');
